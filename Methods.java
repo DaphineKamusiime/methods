c90fd1d2f246f394382c929c02a372ed98799087
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2022-10-01 22:25:28 -0700

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,11 @@
+public class Methods{
+    static void student(String name,int age){
+        /*name is a parameter */
+        System.out.println(name + " is "+ age);
+    }
+    public static void main(String[] args){
+        student("Daphine",20);
+        student("Saara",20);
+        /*daphine and saara are arguments */
+    }
+}
---
+++ Methods2.java
@@ -0,0 +1,8 @@
+public class Methods2 {
+    static int summation(int x){
+        return x+189;
+    }
+    public static void main(string[] args){
+        System.out.println((summation(98)));
+    }
+}
